7d8db0b292e1eaccb23ade1d64314d45a890de81
Manikandan <mtiruman@buffalo.edu>
2015-11-04 15:11:45 -0500

    Finished plot for Kmeans algorithm

--- cluster.py
+++ cluster.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 from sklearn.decomposition import PCA as sklearnPCA
+from sklearn.preprocessing import StandardScaler
 from scipy.cluster.hierarchy import dendrogram, linkage
 from matplotlib import pyplot as plt
 
@@ -38,8 +39,31 @@ def kmeans():
 
         for k in range(0,yeast_k):
             yeastCentroid[k] = [float(sum(l))/len(l) for l in zip(*yeast_c[k])]
-    print "The new yeast Centroid values\n"
-    print yeastCentroid
+    #print "The new yeast Centroid values\n"
+    #print yeastCentroid
+
+    #print "The cluster sizes are - "
+    print len(yeast_c[0]), len(yeast_c[1]), len(yeast_c[2]), len(yeast_c[3]), len(yeast_c[4]), len(yeast_c[5])
+    clusters = np.zeros([614, 7], dtype=float)
+    prev_len = 0
+    for i in range(0,6):
+        for j in range(0,len(yeast_c[i])):
+            clusters[prev_len] = yeast_c[i][j]
+            prev_len += 1
+
+    sklearn_pca = sklearnPCA(n_components = 2)
+    transf = sklearn_pca.fit_transform(clusters)
+    plt.plot(transf[0:140, 0], transf[0:140, 1],'*', markersize = 7, color='blue', alpha=0.5, label='cluster 1')
+    plt.plot(transf[140:191, 0], transf[140:191, 1],'*', markersize = 7, color='red', alpha=0.5, label='cluster 2')
+    plt.plot(transf[191:355, 0], transf[191:355, 1],'*', markersize = 7, color='green', alpha=0.5, label='cluster 3')
+    plt.plot(transf[355:376, 0], transf[355:376, 1],'*', markersize = 7, color='indigo', alpha=0.5, label='cluster 4')
+    plt.plot(transf[376:538, 0], transf[376:538, 1],'*', markersize = 7, color='yellow', alpha=0.5, label='cluster 5')
+    plt.plot(transf[538:614, 0], transf[538:614, 1],'*', markersize = 7, color='black', alpha=0.5, label='cluster 6')
+    plt.xlim([-10, 10])
+    plt.ylim([-10, 10])
+    plt.legend()
+    plt.title("Kmeans")
+    plt.show()
 
 def hcluster():
     exData = np.empty([22, 8], dtype = float)
@@ -70,4 +94,4 @@ def hcluster():
     plt.show()
     """
 kmeans()
-hcluster()
+#hcluster()
